b29b2068140d891fa2c6985d23a5b5dbdd836a44
lifangzheng <1397118980@qq.com>
2025-04-23 09:35:01 +0800

    feat: 更新

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,5 +1,5 @@
-import {defineConfig} from 'wxt'
-import type {WxtViteConfig} from 'wxt'
+import { defineConfig } from 'wxt'
+import type { WxtViteConfig } from 'wxt'
 
 // vite配置
 const viteConfig: WxtViteConfig = {
@@ -27,13 +27,13 @@ const manifest = {
     description: '这是一个 wxt-dev 的开发模板',
     permissions: ['activeTab', 'tabs', 'sidePanel'],
     host_permissions: ['<all_urls>'],
-    // icons: {
-    //     '16': 'icon/16.png',
-    //     '32': 'icon/32.png',
-    //     '48': 'icon/48.png',
-    //     '96': 'icon/96.png',
-    //     '128': 'icon/128.png'
-    // },
+    icons: {
+        '16': 'icon/16.png',
+        '32': 'icon/32.png',
+        '48': 'icon/48.png',
+        '96': 'icon/96.png',
+        '128': 'icon/128.png'
+    },
     action: {
         default_title: 'Click Me'
     },
@@ -47,7 +47,8 @@ export default defineConfig({
     srcDir: 'src',
     entrypointsDir: 'entries',
     extensionApi: 'chrome',
-    publicDir: 'public',
+    // 公共资源目录
+    publicDir: '../public',
     outDir: 'dist',
     modules: ['@wxt-dev/module-react'],
     vite: () => viteConfig,
